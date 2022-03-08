74cc3d10eda5021ce8090a5f5701fd569e0360a7
Slinet6056 <slinet6056@foxmail.com>
2022-03-08 17:07:30 +0800

    Disable plugin telescope-media-files

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -83,7 +83,7 @@ return packer.startup(function(use)
 
 	-- Telescope
 	use("nvim-telescope/telescope.nvim")
-	use("nvim-telescope/telescope-media-files.nvim")
+	-- use("nvim-telescope/telescope-media-files.nvim")
 
 	-- Treesitter
 	use({
